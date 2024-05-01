b37c4792d96c65fc6d43c02986ebc1e0ebdd8bf6
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2024-05-01 10:05:02 -0600

    Small Changes

--- Frontend/WebsiteView/routes/users.js
+++ Frontend/WebsiteView/routes/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 
+const APIPORT = 15000;
 const USERAPIHOST = 'localhost';
-const USERAPIPORT = '';
+
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   try {
-    const response = await axios.get('http://' + USERAPIHOST +':' + USERAPIPORT + '/userapi/');
+    const response = await axios.get('http://' + USERAPIHOST +':' + APIPORT + '/userapi/');
     res.json(response.data)
   } catch (error) {
     res.status(500).json({ error: error.message })
