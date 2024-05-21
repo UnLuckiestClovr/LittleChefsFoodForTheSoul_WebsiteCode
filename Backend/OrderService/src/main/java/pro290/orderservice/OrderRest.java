36c2df7338e246276fa48ec7377298afb61c4137
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2024-05-21 11:51:10 -0600

    Basket Finished and Fixed Double Route

--- Backend/OrderService/src/main/java/pro290/orderservice/OrderRest.java
+++ Backend/OrderService/src/main/java/pro290/orderservice/OrderRest.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("")
 public class OrderRest
 {
     @Autowired
--- Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
+++ Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6768e1cf4875f25ffe3197ba619dd2c9ec315d67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056fdd03b37222940eb6dfd8d37d9abae8623b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
