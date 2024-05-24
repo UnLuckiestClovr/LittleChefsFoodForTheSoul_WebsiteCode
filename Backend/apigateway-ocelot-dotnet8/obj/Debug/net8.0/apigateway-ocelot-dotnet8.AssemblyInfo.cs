3b36d143bfa7907fa01e47e068809ac8d3761bc1
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2024-05-24 12:53:34 -0600

    Added pug pages

--- Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
+++ Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c2df7338e246276fa48ec7377298afb61c4137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c850efe97e9fadfddc085d8abb320b59ac6963")]
 [assembly: System.Reflection.AssemblyProductAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
