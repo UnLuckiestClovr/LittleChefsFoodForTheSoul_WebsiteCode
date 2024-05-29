3e82269fcb39c6b2cc67f3219ee440b7e179049c
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2024-05-28 18:10:04 -0600

    DockerFiles are broken for Java

--- Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
+++ Backend/apigateway-ocelot-dotnet8/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38973006b327b86b7c052d1bfc92c87dba2c147a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d0324e4c42f9cac34bb3af2349e468da368ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
