3850683bf5882c80f3dd24bb3a0c0a62ae4ad817
UnLuckiestClovr <urluckyclovr@gmail.com>
2024-05-28 18:09:45 -0600

    GAMER

--- Frontend/WebsiteView/routes/basketpage.js
+++ Frontend/WebsiteView/routes/basketpage.js
@@ -2,20 +2,26 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 
+GATEWAYHOST = "localhost";
+
 router.get('/', async function(req, res, next) {
-    let boolLog = false
-    if(req.session.user === null || req.session.user === undefined) {
-        boolLog = false
-        res.render("loginorregister", { title: 'Little Chefs', loggedIn:boolLog })
-    } else {
-        const response = {data: null}
-        boolLog = (req.session.user !== null && req.session.user !== undefined)
-        if (req.session.BID === null || req.session.BID === undefined) {
-            response = await axios.post('http://' + GATEWAYHOST +':15010/basket/create', req.body);
+    try {
+        let boolLog = false;
+        if (req.session.user === null || req.session.user === undefined) {
+            boolLog = false;
+            res.render("loginorregister", { title: 'Little Chefs', loggedIn: boolLog });
         } else {
-            response = await axios.get(`http://${GATEWAYHOST}:15010/basket/${req.session.BID}`, req.body);
+            boolLog = (req.session.user !== null && req.session.user !== undefined);
+            let response;
+            if (req.session.user.BID === null || req.session.user.BID === undefined) {
+                response = await axios.post(`http://${GATEWAYHOST}:15010/basket/create`, req.body);
+            } else {
+                response = await axios.get(`http://${GATEWAYHOST}:15010/basket/${req.session.user.BID}`);
+            }
+            res.render('basket', { title: 'Little Chefs', loggedIn: boolLog, basketData: response.data });
         }
-        res.render('basket', { title: 'Little Chefs', loggedIn : boolLog, basketData : response.data });
+    } catch (error) {
+        console.log(error);
     }
 });
 
